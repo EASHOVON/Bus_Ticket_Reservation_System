04f460eab2e574dad4ff4640f0262da64a3bd7ad
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-10-30 20:03:54 +0600

    Added a Bus Class

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ class User:
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+class Bus:
+    def __init__(self, coach, driver, arrival, departure, from_des, to):
+        self.coach = coach
+        self.driver = driver
+        self.arrival = arrival
+        self.from_des = from_des
+        self.to = to
+        self.departure = departure
+        self.seat = ["Empty" for i in range(20)]
