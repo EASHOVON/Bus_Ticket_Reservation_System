eebce31352feab567a40be1356743153bfac63e3
eashovon <ashrafuzzaman.shovon.786@gmail.com>
2022-10-30 21:12:12 +0600

    Added a method that will install a bus

--- main.py
+++ main.py
@@ -13,3 +13,25 @@ class Bus:
         self.to = to
         self.departure = departure
         self.seat = ["Empty" for i in range(20)]
+
+class ShohagBusCompany:
+    total_bus = 5
+    total_bus_lst = []
+
+    def install(self):
+        bus_no = int(input("Enter Bus No : "))
+        flag = 1
+        for w in self.total_bus_lst:
+            if bus_no == w['coach']:
+                print("BUS already installed")
+                flag = 0
+                break
+        if flag:
+            bus_driver = input("Enter Bus Driver Name : ")
+            bus_arrival = input("Enter Bus Arrival Time : ")
+            bus_departure = input("Enter Bus Departure Time : ")
+            bus_from = input("Enter Bus Start From : ")
+            bus_to = input("Enter Bus To Destination : ")
+            self.new_bus = Bus(bus_no, bus_driver, bus_arrival,bus_departure, bus_from, bus_to)
+            self.total_bus_lst.append(vars(self.new_bus))
+            print("\nBus successfully installed")
